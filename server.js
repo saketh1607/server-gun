a94f6b7cbafd7b5c3ee3dfb1198ad4d97b67f780
saketh <87963272+saketh1607@users.noreply.github.com>
2024-12-21 16:50:32 +0530

    Update server.js

--- server.js
+++ server.js
@@ -127,7 +127,7 @@ class GameServer {
         console.log(`Bearing to target: ${bearingToTarget}, Angle difference: ${angleDiff}`);
     
         // Define hit criteria (example)
-        const isHit = distance < 10 && angleDiff > 165 && angleDiff < 220;
+        const isHit = angleDiff > 130&& angleDiff < 250;
         console.log(`Is hit: ${isHit}`);
         return isHit;
     }
